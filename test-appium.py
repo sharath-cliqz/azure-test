02b33b7a1ebc4c1f0864a2ae8d21aa38204c0101
sharath-cliqz <sharath@cliqz.com>
2018-10-27 20:58:53 +0200

    Update test-appium.py

--- test-appium.py
+++ test-appium.py
@@ -44,8 +44,8 @@ if __name__ == "__main__":
 		print(e)
 	finally:
 		driver.quit()
-		sleep(5)
+		sleep(10)
 		co("adb -s emulator-5554 emu kill".split(" "))
-		appium.terminate()
-		emulator.terminate()
+		#appium.terminate()
+		#emulator.terminate()
 		sys.exit()
